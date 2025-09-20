f00c4237125d1753492c78c081be09706747966d
wangliwei <1017496901@qq.com>
2025-09-21 01:14:10 +0800

    Main

---
+++ A.java
@@ -0,0 +1,6 @@
+/**
+ * @author: 10174
+ * @date: 2025/9/21 1:13
+ * @description:
+ */public class A {
+}
---
+++ B.java
@@ -0,0 +1,7 @@
+/**
+ * @author: 10174
+ * @date: 2025/9/21 1:13
+ * @description:
+ */
+public class B {
+}
